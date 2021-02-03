b29b86cc8fe17807f74e1ff901bb3653a996c337
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-02-02 22:59:58 -0500

    debug

--- preproc.py
+++ preproc.py
@@ -116,9 +116,8 @@ def collate_vae(data):
         #ddeltas = get_deltas(deltas)
         #feature = torch.cat([mfcc, deltas, ddeltas], dim=1).squeeze(0)
         feature = get_melspec(audio)
-        print("Before", feature.shape)
         batch_audio = nn.ZeroPad2d(padding=(0, maxlen-feature.shape[1], 0, 0))(feature)
-        print("After", batch_audio.shape)
+        print(feature.shape, batch_audio.shape, maxlen)
         batch.append(batch_audio)
 
     return torch.stack(batch).unsqueeze(1)
