c62c92ef888c97961e98c13fba190cd893bd1937
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2021-03-09 15:51:25 -0500

    check encoder

--- encoder.py
+++ encoder.py
@@ -13,21 +13,18 @@ class Encoder(nn.Module):
         self.conv_pre = Conv(layers=2, stride=1, kernel=3, hid_dim = hid_dim, in_dim=in_dim)
         self.conv_strided = Conv(layers=1, stride=2, kernel=4, hid_dim = hid_dim, residual=False)
         self.conv_post = Conv(layers=2, stride=1, kernel=3, hid_dim = hid_dim)
-        #self.dense = Dense(layers=4, hid_dim=hid_dim)
-        #self.out = nn.Linear(in_features=hid_dim, out_features=enc_dim)
+        self.dense = Dense(layers=4, hid_dim=hid_dim)
+        self.out = nn.Linear(in_features=hid_dim, out_features=enc_dim)
 
     def forward(self, x):
         x = self.conv_pre(x)
-        print("Before stride:",x.shape)
         x = self.conv_strided(x)
-        print("After stride:",x.shape)
         x = self.conv_post(x)
-        print("Conv after stride:", x.shape)
         x = x.permute(0,2,1)
-        #x = self.dense(x)
-        #x = self.out(x)
-        #out = x.permute(0, 2, 1)
-        #print("output shape:", out.shape)
+        x = self.dense(x)
+        x = self.out(x)
+        out = x.permute(0, 2, 1)
+        print("output shape:", out.shape)
         return x
 
 
